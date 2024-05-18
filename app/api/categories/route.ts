86ad6d4a2082dd198e7b53e6cdd8408cef43433b
funkysys <aadelbos@gmail.com>
2024-05-18 14:53:24 +0200

    add headers and cors

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -3,21 +3,12 @@
 // import { Category } from "@/utils/types";
 import { NextRequest, NextResponse } from "next/server"
 import prisma from "@/lib/connect";
-import { NextApiRequest, NextApiResponse } from "next";
 
 
 // GET POST PUT
-export const GET = async (req: NextApiRequest,res: NextApiResponse) => {
+export const GET = async (req: Request,res: Response) => {
 
-    if (req.method === 'OPTIONS') {
-        res.setHeader('Access-Control-Allow-Origin', 'https://discophiles.vercel.app'); // Remplacez par votre domaine autorisé
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        res.status(200).end();
-        return;
-      } else {
-        res.setHeader('Access-Control-Allow-Origin', 'https://example.com'); // Remplacez par votre domaine autorisé
-      }
+
     try {
         const categories = await prisma.category.findMany();
         return NextResponse.json(categories, { status: 200 });
