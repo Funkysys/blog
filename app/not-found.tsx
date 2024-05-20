849f6c83afba847093301cd35c994b32e427b4bb
funkysys <aadelbos@gmail.com>
2024-05-20 17:13:54 +0200

    add reel categories for responsiveMenu

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ const ErrorPage = () => {
         <main className='w-full h-full flex flex-col justify-center items-center'>
             
         <section className='mb-10'>
-            <h2>This Page doesn't exist...</h2>
+            <h2>{`This Page doesn't exist...`}</h2>
         </section>
     </main>
     )
