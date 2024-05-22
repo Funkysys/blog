516ce05b0017365a38043170cd3e801543f6f77d
Funkysys <aadelbos@gmail.com>
2024-05-22 18:53:54 +0200

    add control for email send and register

--- components/EmailForm.tsx
+++ components/EmailForm.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { emailSend } from "@/app/api/email/email.action";
-import { useEmails } from "@/hook/useEmail";
 import { Email } from "@prisma/client";
 import axios from "axios";
 import { FormEventHandler, useState } from "react";
@@ -10,48 +9,55 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 export const EmailForm = () => {
+  const [email, setEmail] = useState("");
   const [emailIsSend, setEmailIsSend] = useState(false);
   const [emailExist, setEmailExist] = useState(false);
-  const { data } = useEmails();
+  const [loader, setLoader] = useState(false);
+
   const createEmail = (newEmail: Partial<Email>) =>
     axios.post("/api/email", newEmail).then((res) => res.data);
-  const { mutate, isLoading } = useMutation(createEmail, {
+
+  const {
+    mutate,
+    isLoading,
+    data: mutateEmail,
+  } = useMutation(createEmail, {
     onSuccess: (data: Email) => {
-      console.log(data);
+      console.log("Welcome to Discopholies !");
     },
   });
 
+  const handleOnChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
   const submitEmail: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    try {
-      const formData = new FormData(e.currentTarget);
+    const { data } = await axios.get(`/api/email/${email}`);
+    setEmailExist(data ? true : false);
 
-      if (
-        data?.find(
-          (email: { email: string }) =>
-            email.email === e.currentTarget.email.value
-        )
-      ) {
-        return setEmailExist(true);
-      }
-      await mutate({
-        email: e.currentTarget.email.value as string,
-      });
-      const res = await emailSend(formData);
-      console.log(res);
-
-      setEmailIsSend(true);
+    if (emailExist) {
       return;
-    } catch (error) {
-      console.error("Error in uploadImage : ", error);
+    }
+    if (emailExist && email.length > 0) {
+      try {
+        const formData = new FormData(e.currentTarget);
+
+        await mutate({
+          email: email,
+        });
+        if (mutateEmail) {
+          const res = await emailSend(formData);
+          setEmailIsSend(true);
+        }
+
+        return;
+      } catch (error) {
+        console.error("Error in sendEmail : ", error);
+      }
     }
   };
-  if (emailExist)
-    return (
-      <div className="p-4 bg-gray-200 rounded-lg">
-        <p className="text-green-800">Email already exist !!!</p>
-      </div>
-    );
+
   if (emailIsSend)
     return (
       <div className="p-4 bg-gray-200 rounded-lg">
@@ -60,12 +66,25 @@ export const EmailForm = () => {
         </p>
       </div>
     );
+  if (emailExist)
+    return (
+      <div className="p-4 mt-2 bg-gray-200 rounded-lg">
+        <p className="text-green-800">Email already exist...</p>
+      </div>
+    );
+  if (isLoading)
+    return (
+      <div className="p-4 mt-2 bg-gray-200 rounded-lg">
+        <p className="text-green-800">Something happens...</p>
+      </div>
+    );
   return (
     <form onSubmit={submitEmail}>
       <Input
         id="email"
         name="email"
         type="email"
+        onChange={handleOnChange}
         placeholder="Your email..."
         className=" mt-5"
         required
--- hook/useEmail.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { useQuery } from "react-query";
-
-export const useEmails = () => {
-  return useQuery("emails", async () => {
-    const { data } = await axios.get("/api/emails");
-    return data;
-  });
-};
