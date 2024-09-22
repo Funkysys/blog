2c2018cf6742b7410648117bb0d5152333b18a09
funkysys <aadelbos@gmail.com>
2024-09-22 17:36:05 +0200

    add paypal button

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,6 +9,19 @@ const Footer = () => {
   const router = useRouter();
   return (
     <footer className="p-4 border-t">
+      <div className="w-full h-20vh bg-blue-950 text-slate-900 rounded-md flex justify-center p-4">
+        <Button
+          variant="outline"
+          className="bg-amber-400"
+          onClick={() =>
+            router.push(
+              "https://www.paypal.com/donate?hosted_button_id=64QKCN79A994E"
+            )
+          }
+        >
+          Support us on paypal
+        </Button>
+      </div>
       <div className="flex items-center justify-between">
         <FirstTitle />
         <div className="flex items-center justify-center m-5 p-5 w-full">
