a31f0cbc0172020c88a81aaa069fe77166bd115f
funkysys <aadelbos@gmail.com>
2024-09-23 16:31:55 +0200

    animation home page

--- components/LandingPage/HomeComponent.tsx
+++ components/LandingPage/HomeComponent.tsx
@@ -22,22 +22,23 @@ const HomeComponent = () => {
     );
   }, []);
   return (
-    <div
-      ref={containerRef}
-      style={{ backgroundImage: "url(/img/disque.jpg)", opacity: 0 }}
-      className="rounded-full w-[90%] md:w-[50%] m-auto aspect-square md:aspect-[1/1] overflow-hidden bg-cover "
-    >
-      <div className="h-full w-full flex flex-col justify-center items-center">
+    <div className="relative w-100%">
+      <div
+        ref={containerRef}
+        style={{ backgroundImage: "url(/img/disque.jpg)", opacity: 0 }}
+        className="rounded-full w-[90%] md:w-[50%] m-auto aspect-square md:aspect-[1/1] overflow-hidden bg-cover relative motion-safe:animate-spin hover:animate-out"
+      ></div>
+      <div className="absolute top-0 h-full w-full flex flex-col justify-center items-center">
         <div className="absolute w-96 h-96 bg-slate-600/80 dark:bg-slate-800/80 p-4 rounded-full flex flex-col justify-center items-center">
-          <div className="relative flex justify-center items-center">
+          <div className="relative flex justify-center items-center ">
             <Disc
-              className=" mr-2 rounded-full bg-gradient-to-br from-yellow-900 to-green-300 text-white border-2 border-gray-400
-            "
+              className=" mr-2 rounded-full bg-gradient-to-br from-yellow-900 to-green-300 text-white border-2 border-gray-400 
+              "
             />
             <h1
               className="text-center font-bold text-3xl md:text-5xl 
-            text-transparent bg-clip-text bg-gradient-to-br from-yellow-200 to-green-300
-            "
+              text-transparent bg-clip-text bg-gradient-to-br from-yellow-200 to-green-300
+              "
             >
               DiscoPhiles
             </h1>
