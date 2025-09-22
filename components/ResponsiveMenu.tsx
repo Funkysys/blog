07dc79b60ea6e146e9d149fd81563ca736c3d038
Antoine Delbos <aadelbos@gmail.com>
2025-09-22 16:29:24 +0200

    del categories from responsive nav

--- components/ResponsiveMenu.tsx
+++ components/ResponsiveMenu.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useCategories } from "@/hook/useCategories";
-import { User } from "@/types";
+import { Category, User } from "@/types";
 import axios from "axios";
 import { Menu } from "lucide-react";
 import { signOut, useSession } from "next-auth/react";
@@ -135,6 +135,25 @@ const ResponsiveMenu = () => {
                 </Button>
               </>
             )}
+          </div>
+          <div className="hidden md:flex flex-col gap-4 w-full text-center ">
+            <p className=" underline text-lg">Categories</p>
+            {!isLoading &&
+              categories?.map((category: Category) => (
+                <Link
+                  key={category.id}
+                  href={`/categories/${category.slug}`}
+                  className="block px-2 py-1 text-lg"
+                >
+                  <Button
+                    className="bg-slate-700 hover:bg-slate-300 text-slate-50"
+                    variant={"ghost"}
+                    onClick={() => setOpenMenu(false)}
+                  >
+                    {category.title}
+                  </Button>
+                </Link>
+              ))}
           </div>
         </SheetContent>
       </Sheet>
